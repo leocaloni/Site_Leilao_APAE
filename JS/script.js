5074576a63a3001c5e991f0e7f6fe6d2f21cef53
Leonardo Caloni Munduruca <leomunduruca@gmail.com>
2023-11-21 18:48:07 -0300

    Pagina produto decorada

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,43 @@ btnPopup.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+
+// script para digitar valor do lance
+
+document.addEventListener("DOMContentLoaded", function () {
+  const valorInput = document.getElementById("valor");
+
+  // Adicionar "R$" inicialmente
+  if (!valorInput.value.trim() || valorInput.value === "R$") {
+    valorInput.value = "R$ ";
+  }
+
+  // Adicionar "R$" quando o campo estiver em foco
+  valorInput.addEventListener("focus", function () {
+    if (!valorInput.value.trim() || valorInput.value === "R$") {
+      valorInput.value = "R$ ";
+    }
+  });
+
+  // Adicionar "R$" quando o campo perder o foco, se estiver vazio
+  valorInput.addEventListener("blur", function () {
+    if (!valorInput.value.trim()) {
+      valorInput.value = "R$ ";
+    }
+  });
+
+  // Permitir apenas números, vírgula e ponto decimal
+  valorInput.addEventListener("input", function () {
+    valorInput.value = valorInput.value.replace(/[^\d,\.]/g, "");
+
+    // Adicionar "R$" durante a digitação se necessário
+    if (!valorInput.value.includes("R$")) {
+      valorInput.value = "R$ " + valorInput.value;
+    }
+  });
+});
+
+
+
+
